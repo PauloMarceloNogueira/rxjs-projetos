fe9704bb29b9adf631ac49ba21d8b47ab22649f1
Paulo Marcelo <paulomarceloa.nogueira@gmail.com>
2018-10-27 18:52:39 -0300

    Include Search

--- src/Observables/keyup.js
+++ src/Observables/keyup.js
@@ -7,15 +7,11 @@ import {
 } from 'rxjs/operators';
 import 'rxjs/add/observable/dom/ajax';
 
-const listResult = document.getElementById('result');
 const input = document.getElementById('input');
 
 export default fromEvent(input, 'keyup')
   .pipe(
-    map((event) => {
-      listResult.innerHTML = '';
-      return event.target.value.toUpperCase() || 0;
-    }),
+    map(event => event.target.value.toUpperCase() || 0),
     debounceTime(500),
     distinctUntilChanged(),
   );
--- src/index.js
+++ src/index.js
@@ -1,20 +1,36 @@
 /* eslint no-undef: "off" */
 /* eslint no-console: "off" */
 import { Observable, merge } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, withLatestFrom, filter } from 'rxjs/operators';
 import 'rxjs/add/operator/switchMap';
 import keyup$ from './Observables/keyup';
-import click$ from './Observables/pagination';
+import pagination$ from './Observables/pagination';
+// import { filter } from '../node_modules/rxjs-compat/operator/filter';
+
+const restResponse$ = Observable.create((obs) => {
+  obs.next('Created!');
+});
 
 const listResult = document.getElementById('result');
 
 const searchAPI = page => Observable.ajax(`https://reqres.in/api/users?page=${page}&per_page=5`);
 
-// const searcher$ = keyup$.switchMap(searchAPI);
 const searcher$ = merge(
-  click$.switchMap(searchAPI),
-  keyup$.switchMap(searchAPI),
+  restResponse$.switchMap(searchAPI),
+  pagination$.switchMap(searchAPI),
 );
+
+keyup$.pipe(
+  withLatestFrom(searcher$, (therm, result) => ({ therm, result })),
+  map(
+    ({ therm, result }) => result.response.data.filter(a => a.first_name.toUpperCase() === therm),
+  ),
+).subscribe((a) => {
+  const list = document.createElement('li');
+  listResult.innerHTML = '';
+  list.innerHTML = `${a[0].id} | ${a[0].first_name}`;
+  listResult.appendChild(list);
+});
 
 searcher$.pipe(
   map((val) => {
